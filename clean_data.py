c798640930a7f5c05564053ccbdd2e41a1a3643f
violet <treefrog@riseup.net>
2016-01-04 17:10:55 +0000

    print national data to ascii table

--- clean_data.py
+++ clean_data.py
@@ -2,14 +2,13 @@
 
 # you need to install beautifulsoup, numpy, and pandas
 from bs4 import BeautifulSoup
-import numpy as np
 import pandas as pd
 
 
 def get_national_data(data):
     header = [ td.get_text().strip('\xa0') for td in data.find_all("tr")[0].find_all("td") ]
-
-    header[header.index('Nominees'):header.index('Nominees')+1] = [ td.get_text().strip('\xa0') for td in data.find_all("tr")[1].find_all("td") ]
+    header[header.index('Nominees'):header.index('Nominees')+1] = \
+        [ td.get_text().strip('\xa0') for td in data.find_all("tr")[1].find_all("td") ]
     header.append("Year")
     while True:
         try:
@@ -43,12 +42,9 @@ for year in range(1824, 2016, 4):
 #    else:
 #        national_html = soup.find_all("table", attrs={"class":"elections_states"})[0]
     national_html = soup.find_all("table", attrs={"class":"elections_states"})[0]
-
     if national_html.find_all("table", attrs={"class":"elections_states"}): 
         national_html = national_html.find_all("table", attrs={"class":"elections_states"})[0]
-
     file.close()
-
     if national_frame.empty:
         national_frame = get_national_data(national_html)
     else:
@@ -58,8 +54,10 @@ for year in range(1824, 2016, 4):
             print("Error: ", year)
 
 print(national_frame)
+national_frame.to_csv('data/national_data.txt', sep='\t', index=False)
 
 #########################
+
 #year = 1820
 #file = open("raw_data/results_" + str(year) + ".html", "r")
 #soup = BeautifulSoup(file, 'html.parser')
