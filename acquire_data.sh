e9aa665f322754b01337b6dbc8f5d16fc35f3222
violet <treefrog@riseup.net>
2016-01-04 15:10:47 +0000

    change first year to 1824 because webiste being scraped doesn't have pop. vote data for elections before 1824

--- acquire_data.sh
+++ acquire_data.sh
@@ -2,7 +2,7 @@
 
 BASEURL="http://www.presidency.ucsb.edu/showelection.php?year="
 
-for YEAR in $(seq 1796 4 2012); do
+for YEAR in $(seq 1824 4 2012); do
 	torify wget "$BASEURL""$YEAR" -O raw_data/results_$YEAR.html
 	sed -i "s///g" raw_data/results_$YEAR.html
 done
