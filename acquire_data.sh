a6174fd83eca28b55454effab354dd102ff904c6
violet <treefrog@riseup.net>
2016-01-05 18:01:29 +0000

    switch to new data source

--- acquire_data.sh
+++ acquire_data.sh
@@ -11,7 +11,8 @@
 BASE_URL="http://uselectionatlas.org"
 for YEAR in $(seq 1824 4 2012); do
 	NATIONAL_URL="${BASE_URL}/RESULTS/national.php?year=${YEAR}&off=0&f=1"
-	STATE_URL="${BASE_URL}/RESULTS/data.php?ev=1&per=1&vot=1&sort=&fips=0&search=&search_name=&datatype=national&f=0&off=0&year=${YEAR}&sort_dir=&submit=Submit"
-	curl "$NATIONAL_URL" -o raw_data/national_results_${YEAR}.html
+#	STATE_URL="${BASE_URL}/RESULTS/data.php?ev=1&per=1&vot=1&sort=&fips=0&search=&search_name=&datatype=national&f=0&off=0&year=${YEAR}&sort_dir=&submit=Submit"
+	STATE_URL="${BASE_URL}/RESULTS/data.php?ev=1&vot=1&sort=&fips=0&search=&search_name=&datatype=national&f=0&off=0&year=${YEAR}&sort_dir=&submit=Submit"
+#	curl "$NATIONAL_URL" -o raw_data/national_results_${YEAR}.html
 	curl "$STATE_URL" -o raw_data/state_results_${YEAR}.html
 done
