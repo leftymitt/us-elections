b3c4360617dc6baa04792c60afa583e812b50b02
violet <treefrog@riseup.net>
2016-01-06 02:35:38 +0000

    finished python clean-up script

--- clean_data.py
+++ clean_data.py
@@ -3,6 +3,7 @@
 # you need to install beautifulsoup and pandas
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 
 def get_national_data(data, year):
@@ -19,34 +20,55 @@ def get_national_data(data, year):
 
 
 def get_state_data(data, year):
-    header = [ 'State', 'PresidentialCandidate', 'Electoral Vote', 'Popular Vote', 'Year' ]
-    frame = pd.DataFrame(columns=header)
-    num_candidates = (len([ td.get_text() for td in data[0].find_all("td") ][1:])-3)//2
-    candidates = [ td.get_text() for td in data[0].find_all("td") ][1+num_candidates+2:]
+    fields = [ 'State', 'PresidentialCandidate', 'Electoral Vote', 'Popular Vote', 'Year' ]
+    frame = pd.DataFrame(columns=fields)
+    header = [ td.get_text() for td in data[0].find_all("td") ][1:]
+    header_midpoint = header.index('Total\xa0Vote')+1
+    num_candidates = len(header[header_midpoint:]) 
+    candidates = header[header_midpoint:]
+    num_ev = len(header[1:header_midpoint-1])
     for row in range(0, len(data[1].find_all("tr"))-1):
         row = [ td.get_text().strip('\xa0') for td in data[1].find_all("tr")[row].find_all("td") ][1:]
-        for idx in range(0, num_candidates):
-            content = [ row[0], candidates[idx], row[1+idx], row[2+num_candidates+idx], year]
-            frame = frame.append(pd.Series(content, header), ignore_index=True)
-        content = [ row[0], candidates[num_candidates], '0', row[len(row)-1], year ]
-        frame = frame.append(pd.Series(content, header), ignore_index=True)
+        for idx, candidate in enumerate(candidates):
+            if idx < num_ev:
+                content = [ row[0], candidate, row[1+idx], row[2+num_ev+idx], year]
+            else: 
+                content = [ row[0], candidate, '0', row[2+num_ev+idx], year]
+            frame = frame.append(pd.Series(content, fields), ignore_index=True)
     return frame
 
 
+# add full candidate name and the political party
+def fix_data(national, state):
+    candidate = []
+    party = []
+    years = sorted(set(state['Year']))
+    for year in years: 
+        for name in state['PresidentialCandidate'][state['Year'] == year]:
+            full_names = national['PresidentialCandidate'][national['Year'] == year]
+            test = [ full_name for full_name in full_names if re.search(name, full_name) ]
+            if test: 
+                candidate.append(test[0])
+                party.append(national['PoliticalParty'][full_names[full_names == test[0]].index[0]])
+            else:
+                candidate.append(name)
+                party.append('-')
+    state['PresidentialCandidate'] = pd.Series(candidate)
+    state['PoliticalParty'] = pd.Series(party)
+
+# clean up data
 national_frame = pd.DataFrame;
 state_frame = pd.DataFrame;
 for year in range(1824, 2016, 4):
-#    print(year)
+    print("processing election:", year)
     file = open("raw_data/national_results_" + str(year) + ".html", "r")
     soup = BeautifulSoup(file, 'html.parser')
     national_html = [ soup.find_all("thead")[0], soup.find_all("tbody")[0] ]
     file.close()
-    
     file = open("raw_data/state_results_" + str(year) + ".html", "r")
     soup = BeautifulSoup(file, 'html.parser')
     state_html = [ soup.find_all("thead")[0], soup.find_all("tbody")[0] ]
     file.close()
-
     if national_frame.empty:
         national_frame = get_national_data(national_html, year)
     else:
@@ -54,7 +76,6 @@ for year in range(1824, 2016, 4):
             national_frame = national_frame.append(get_national_data(national_html, year), ignore_index=True)
         except ValueError:
             print("Error: ", year)
-
     if state_frame.empty:
         state_frame = get_state_data(state_html, year)
     else:
@@ -63,18 +84,10 @@ for year in range(1824, 2016, 4):
         except ValueError:
             print("Error: ", year)
 
+print("cleaning up...")
+fix_data(national_frame, state_frame)
+
 #print(national_frame)
 #print(state_frame)
 national_frame.to_csv('data/national_data.txt', sep='\t', index=False)
 state_frame.to_csv('data/state_data.txt', sep='\t', index=False)
-
-#########################
-
-#year = 1824
-#file = open("raw_data/national_results_" + str(year) + ".html", "r")
-#file = open("raw_data/state_results_" + str(year) + ".html", "r")
-#soup = BeautifulSoup(file, 'html.parser')
-#national_html = [ soup.find_all("thead")[0], soup.find_all("tbody")[0] ]
-#get_national_data(national_html, year)
-#state_html = soup.find_all("table", attrs={"class":"elections_states"})[1]
-#file.close()
