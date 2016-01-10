2a5f201f3ec30bb7c9b4bb03d232e4df3da1dca9
violet <treefrog@riseup.net>
2016-01-10 05:21:33 +0000

    moved comma-cleaning script into separate file

--- plot_state_data.jl
+++ plot_state_data.jl
@@ -3,18 +3,7 @@
 using Gadfly
 using DataFrames
 
-################################################################################
-# define general utility functions.
-################################################################################
-
-# pandas saves a comma as a thousands-place separator
-function fixdata(arr)
-	result = DataArray(Int, length(arr))
-	for i in 1:length(arr)
-		result[i] = parse(Int, join(split(arr[i], ',')))
-	end
-	return result
-end
+include("utils.jl")
 
 ################################################################################
 # load data.
