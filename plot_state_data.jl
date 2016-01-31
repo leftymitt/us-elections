1ce807999b6e7b5191c7815194c2012c8ebd55e4
violet <treefrog@riseup.net>
2016-01-31 01:57:18 +0000

    add data for geographic regions

--- plot_state_data.jl
+++ plot_state_data.jl
@@ -9,9 +9,11 @@ include("utils.jl")
 ################################################################################
 # load data.
 ################################################################################
+
 state_data = readtable("data/state_data.txt", separator='\t');
 state_data[:Popular_Vote] = fixdata(state_data[:Popular_Vote]);
 rename!(state_data, :PoliticalParty, :Party)
+
 
 ################################################################################
 # calculate popular and electoral vote totals and percents.
@@ -45,9 +47,98 @@ state_data[:Electoral_Percent] =
 	map((vote, total) -> vote / total * 100, state_data[:Electoral_Vote], 
 	    state_data[:Electoral_Total])
 
+
+################################################################################
+# assign each state to a geographic region.
+################################################################################
+
+new_england = 
+	[ "Connecticut", "Maine", "Massachusetts", "New Hampshire", "Rhode Island", 
+	  "Vermont" ]
+mid_atlantic = 
+	[ "New Jersey", "New York", "Pennsylvania" ]
+east_north_central = 
+	[ "Illinois", "Indiana", "Michigan", "Ohio", "Wisconsin" ]
+west_north_central =
+	[ "Iowa", "Kansas", "Minnesota", "Missouri", "Nebraska", "North Dakota",
+	  "South Dakota" ]
+south_atlantic =
+	[ "Delaware", "Florida", "Georgia", "Maryland", "North Carolina", 
+	  "South Carolina", "Virginia", "D. C.", "West Virginia" ]
+east_south_central = 
+	[ "Alabama", "Kentucky", "Mississippi", "Tennessee" ]
+west_south_central = 
+	[ "Arkansas", "Louisiana", "Oklahoma", "Texas" ]
+mountain = 
+	[ "Arizona", "Colorado", "Idaho", "Montana", "Nevada", "New Mexico", "Utah", 
+	  "Wyoming"]
+pacific = 
+	[ "Alaska", "California", "Hawaii", "Oregon", "Washington" ]
+
+northeast = vcat(new_england, mid_atlantic)
+midwest   = vcat(east_north_central, west_north_central)
+south     = vcat(south_atlantic, east_south_central, west_south_central)
+west      = vcat(mountain, pacific)
+
+#state_data[:Region] = "NA"
+#for idx in 1:length(state_data[:State])
+#	println(state_data[:State][idx])
+#	if state_data[:State][idx] in northeast
+#		state_data[:Region][idx] = "Northeast"
+#	elseif state_data[:State][idx] in midwest
+#		state_data[:Region][idx] = "Midwest"
+#	elseif state_data[:State][idx] in south
+#		state_data[:Region][idx] = "South"
+#	elseif state_data[:State][idx] in west
+#		state_data[:Region][idx] = "West"
+#	end
+#end
+
+state_data = 
+	join(state_data, 
+	     (by(state_data, [:State]) do df
+	         if !isempty(intersect(northeast, df[:State]))
+		         "Northeast"
+         	elseif !isempty(intersect(midwest, df[:State]))
+         		"Midwest"
+         	elseif !isempty(intersect(west, df[:State]))
+         		"West"
+         	elseif !isempty(intersect(south, df[:State]))
+         		"South"
+         	end
+         end), on=[:State])
+rename!(state_data, :x1, :Region)
+
+state_data = 
+	join(state_data, 
+	     (by(state_data, [:State]) do df
+	         if !isempty(intersect(west_south_central, df[:State]))
+		         "West South Central"
+         	elseif !isempty(intersect(new_england, df[:State]))
+         		"New England"
+         	elseif !isempty(intersect(mid_atlantic, df[:State]))
+         		"Mid-Atlantic"
+         	elseif !isempty(intersect(east_north_central, df[:State]))
+         		"East North Central"
+         	elseif !isempty(intersect(west_north_central, df[:State]))
+         		"West North Central"
+         	elseif !isempty(intersect(south_atlantic, df[:State]))
+         		"South Atlantic"
+         	elseif !isempty(intersect(east_south_central, df[:State]))
+         		"East South Central"
+         	elseif !isempty(intersect(mountain, df[:State]))
+         		"Mountain"
+         	elseif !isempty(intersect(pacific, df[:State]))
+         		"Pacific"
+         	end
+         end), on=[:State])
+rename!(state_data, :x1, :Division)
+
+
 ################################################################################
 # general plots.
 ################################################################################
+
 for state in groupby(state_data, :State)
 	firstyear = Int(minimum(state[:Year]))-4
 	lastyear = Int(maximum(state[:Year]))+4
@@ -70,9 +161,11 @@ for state in groupby(state_data, :State)
 	draw(SVG(lowercase(strip(string("plots/all_", slug, ".svg"))), 32cm, 16cm), p)
 end
 
+
 ################################################################################
 # democrats and republicans.
 ################################################################################
+
 republican_data = state_data[state_data[:Party] .== "Republican", :]
 democrat_data = state_data[state_data[:Party] .== "Democratic", :]
 bi_state_data = vcat(republican_data, democrat_data)
@@ -100,13 +193,15 @@ for state in groupby(bi_state_data, :State)
 	draw(SVG(lowercase(strip(string("plots/bi_", slug, ".svg"))), 32cm, 16cm), p)
 end
 
+
 ################################################################################
 # all states over time.
 ################################################################################
+
 bi_state_data_1860 = bi_state_data[bi_state_data[:Year] .>= 1860, :]
 bi_state_diff = by( bi_state_data_1860, [:Year, :State], 
-                      df -> df[:Popular_Percent][df[:Party] .== "Republican"] - 
-							       df[:Popular_Percent][df[:Party] .== "Democratic"] )
+                    df -> df[:Popular_Percent][df[:Party] .== "Republican"] - 
+                          df[:Popular_Percent][df[:Party] .== "Democratic"] )
 rename!(bi_state_diff, :x1, :Difference)
 
 firstyear = Int(minimum(bi_state_diff[:Year]))-4
